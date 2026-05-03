9b0f716baaaf0eacbcbfc47b1bdfb9b7f8bda1ec
56webb <skunkqq@gmail.com>
2026-05-03 13:16:57 +0800

    Force Referrer and Origin headers to fix Error 153

--- script.js
+++ script.js
@@ -179,7 +179,7 @@ document.addEventListener('DOMContentLoaded', () => {
             // 使用 autoplay=1 & mute=1 允許自動播放
             cell.innerHTML = `
                 <iframe 
-                    src="https://www.youtube.com/embed/${id}?autoplay=1&mute=1&controls=1&origin=${encodeURIComponent(window.location.origin)}" 
+                    src="https://www.youtube.com/embed/${id}?autoplay=1&mute=1&controls=1&origin=https://www.youtube.com" 
                     title="YouTube video player" 
                     frameborder="0" 
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
